68b2d9b2f370cde7f3527fa9cccc65c45c1cc990
Zubair Ahmed <zubairdotnet@hotmail.com>
2018-02-18 21:55:50 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -151,7 +151,71 @@ def writeLicensePlateCharsOnImage(imgOriginalScene, licPlate):
 
 if __name__ == "__main__":
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    main(dir_path + '/Test_car_images_dataset/538E945.jpg')
-    #main('KDV3675.jpg')
-    #main('BJP139.jpg')
-    #main('TN57AD3604.jpg')
+    main(dir_path + '/Test_images/538E945.jpg')
+    main(dir_path + '/Test_images/BKF196.jpg')
+    main(dir_path + '/Test_images/489T051.jpg')
+
+#Output
+# "C:\Users\Zubair Ahmed\AppData\Local\Programs\Python\Python35\python.exe" "C:/Users/Zubair Ahmed/Downloads/Number-Plate-Recognition-master/Number-Plate-Recognition-master/Main.py"
+# Using TensorFlow backend.
+# 2018-02-18 21:46:56.094382: W C:\tf_jenkins\home\workspace\rel-win\M\windows\PY\35\tensorflow\core\platform\cpu_feature_guard.cc:45] The TensorFlow library wasn't compiled to use AVX instructions, but these are available on your machine and could speed up CPU computations.
+# 1/1 [==============================] - 0s
+# 3
+# 1/1 [==============================] - 0s
+# 8
+# 1/1 [==============================] - 0s
+# E
+# 1/1 [==============================] - 0s
+# 9
+# 1/1 [==============================] - 0s
+# 4
+# 1/1 [==============================] - 0s
+# 5
+# 1/1 [==============================] - 0s
+# 6
+# 1/1 [==============================] - 0s
+# D
+# 1/1 [==============================] - 0s
+# 3
+# 
+# license plate read from  C:\Users\Zubair Ahmed\Downloads\Number-Plate-Recognition-master\Number-Plate-Recognition-master/Test_images/538E945.jpg  : 38E945 
+# 
+# ----------------------------------------
+# 1/1 [==============================] - 0s
+# B
+# 1/1 [==============================] - 0s
+# K
+# 1/1 [==============================] - 0s
+# F
+# 1/1 [==============================] - 0s
+# 1
+# 1/1 [==============================] - 0s
+# 9
+# 1/1 [==============================] - 0s
+# 6
+# 
+# license plate read from  C:\Users\Zubair Ahmed\Downloads\Number-Plate-Recognition-master\Number-Plate-Recognition-master/Test_images/BKF196.jpg  : BKF196 
+# 
+# ----------------------------------------
+# 1/1 [==============================] - 0s
+# 4
+# 1/1 [==============================] - 0s
+# 8
+# 1/1 [==============================] - 0s
+# 9
+# 1/1 [==============================] - 0s
+# T
+# 1/1 [==============================] - 0s
+# 0
+# 1/1 [==============================] - 0s
+# 5
+# 1/1 [==============================] - 0s
+# 7
+# 
+# license plate read from  C:\Users\Zubair Ahmed\Downloads\Number-Plate-Recognition-master\Number-Plate-Recognition-master/Test_images/489T051.jpg  : 489T057 
+# 
+# ----------------------------------------
+# 
+# Process finished with exit code 0
+
+
